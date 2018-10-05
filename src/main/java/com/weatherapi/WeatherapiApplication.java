0581f46b4aeead4c1d3ad4a5f7952d1f00e2624f
João Paulo <vilarjp@info.ufrn.br>
2018-10-04 23:48:58 -0300

    First commit

---
+++ src/main/java/com/weatherapi/WeatherapiApplication.java
@@ -0,0 +1,12 @@
+package com.weatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherapiApplication.class, args);
+	}
+}
